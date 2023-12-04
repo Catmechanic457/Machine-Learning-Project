1494af2b529f879ee2e77939436fdacf143c5f02
Catmechanic457 <tristan.lansdown@icloud.com>
2023-12-04 20:40:54 +0000

    Removed some relience on nn::Values in nn::Network

--- src/lib/NeuralNetwork.cpp
+++ src/lib/NeuralNetwork.cpp
@@ -48,6 +48,12 @@ namespace nn {
         vector<unsigned int> shape;
         vector<double> weights;
         vector<double> bias;
+
+        Values() {}
+        Values(vector<unsigned int> shape_, vector<double> weights_, vector<double> bias_) :
+            shape(shape_),
+            weights(weights_),
+            bias(bias_) {}
     };
 
     class Network {
@@ -56,9 +62,9 @@ namespace nn {
         unsigned int w_index(unsigned int l_, unsigned int n_, unsigned int w_) const {
             unsigned int index = 0;
             for (unsigned int i = 0; i < l_; i++) {
-                index += _values.shape[i] * _values.shape[i+1];
+                index += _shape[i] * _shape[i+1];
             }
-            index += n_ * _values.shape[l_ + 1];
+            index += n_ * _shape[l_+1];
             index += w_;
             return index;
         }
@@ -66,7 +72,7 @@ namespace nn {
         unsigned int b_index(unsigned int l_, unsigned int n_) const {
             unsigned int index = 0;
             for (unsigned int i = 0; i < l_; i++) {
-                index += _values.shape[i];
+                index += _shape[i];
             }
             index += n_;
             return index;
@@ -94,75 +100,80 @@ namespace nn {
             return value_ + bias_;
         }
 
-        unsigned int _w_size;
-        unsigned int _b_size;
+        vector<unsigned int> _shape;
         unsigned int _layers;
 
-        Values _values;
+        vector<double> _weights;
+        unsigned int _w_size;
+
+        vector<double> _bias;
+        unsigned int _b_size;
 
         public:
-        Network(vector<unsigned int> s_) {
-            // Store shape
-            _values.shape = s_;
-            _layers = _values.shape.size();
+        Network(vector<unsigned int> s_) : 
+            _shape(s_),
+            _layers(s_.size())
+            {
 
             // Calculate number of weights
-            _w_size = 0;
-            for (unsigned int i = 0; i < _values.shape.size() - 1; i++) {
-                _w_size += _values.shape[i] * _values.shape[i+1];
+            unsigned int w_size = 0;
+            for (unsigned int i = 1; i < _layers; i++) {
+                w_size += _shape[i-1] * _shape[i];
             }
+            _w_size = w_size;
 
             // Calculate number of bias
-            _b_size = 0;
-            for (unsigned int i = 0; i < _values.shape.size(); i++) {
-                _b_size += _values.shape[i];
+            unsigned int b_size = 0;
+            for (unsigned int i = 0; i < _layers; i++) {
+                _b_size += _shape[i];
             }
+            _b_size = b_size;
 
             // Size weight vector
-            _values.weights.resize(_w_size); _values.weights.shrink_to_fit();
+            _weights.resize(_w_size);
+            _weights.shrink_to_fit();
 
             // Size weight vector
-            _values.bias.resize(_b_size); _values.bias.shrink_to_fit();
-
-            cout << "Initialised:";
-            cout << "\n\tWeights: " << _w_size;
-            cout << "\n\tBias: " << _b_size;
-            cout << "\n";
+            _bias.resize(_b_size);
+            _bias.shrink_to_fit();
         }
         /**
          * \brief The number of nodes in each layer.
          * Index `0` denotes the input layer
          * \return The shape as a vector
         */
-        vector<unsigned int> shape() const {return _values.shape;}
+        vector<unsigned int> shape() const {return _shape;}
         /**
          * \brief The bias of each node in each layer
          * \return The bias as a vector
         */
-        vector<double> bias() const {return _values.bias;}
+        vector<double> bias() const {return _bias;}
         /**
          * \brief The weights of all connections across all layers
          * \return The weights as a vector
         */
-        vector<double> weights() const {return _values.weights;}
+        vector<double> weights() const {return _weights;}
         /**
          * \brief Packages network values into a `nn::Values` object
          * \return The network values
         */
-        Values values() const {return _values;}
-
+        Values package_values() const {
+            Values v(_shape, _weights, _bias);
+            return v;
+        }
         /**
          * \brief loads `nn::Values` into the network
          * The shape of the new values must match the shape of the network
          * \param v_ New network values
         */
-        void load_values(Values v_) {
+        void load_values(Values& v_) {
             // Check shape of new values matches shape of old
-            if (v_.shape != _values.shape) {
+            if (v_.shape != _shape) {
                 throw invalid_argument("Shape of new values does not match shape of the network.");
                 return;
             }
-            _values = v_;
+            _weights = v_.weights;
+            _bias = v_.bias;
         }
 
         // RE-WRITE TO NOT USE b_index & w_index
@@ -172,20 +183,20 @@ namespace nn {
          * \param input_ Values of the input layer as a vector
          * \return Values of the output layer as a vector
         */
-        vector<double> calculate(vector<double> input_) const {
+        vector<double> calculate(const vector<double>& input_) const {
             vector<double> l_inputs = input_;
             vector<double> l_outputs;
             for (unsigned int l = 1; l < _layers; l++) {
                 // For every layer (excluding input layer)
-                l_outputs.resize(_values.shape[l]);
-                for (unsigned int n = 0; n < _values.shape[l]; n++) {
+                l_outputs.resize(_shape[l]);
+                for (unsigned int n = 0; n < _shape[l]; n++) {
                     // For every node in current layer
                     double n_output = 0.0;
-                    double n_bias = _values.bias[b_index(l, n)];
-                    for (unsigned int np = 0; np < _values.shape[l-1]; np++) {
+                    double n_bias = _bias[b_index(l, n)];
+                    for (unsigned int np = 0; np < _shape[l-1]; np++) {
                         // For node in previous layer
                         unsigned int index = w_index(l-1, np, n);
-                        double weight = _values.weights[index]; // Get weight of node
+                        double weight = _weights[index]; // Get weight of node
                         double weighted_input = l_inputs[np] * weight;
                         n_output += weighted_input;
                     }
@@ -195,7 +206,7 @@ namespace nn {
                     else {l_outputs[n] = af_sig(n_output, n_bias);}
                 }
                     l_inputs = l_outputs;
-                    l_inputs.resize(_values.shape[l]);
+                    l_inputs.resize(_shape[l]);
             }
             return l_outputs;
         }
@@ -248,10 +259,7 @@ namespace nn {
             vector<unsigned int> shape = jcv::to_vector<unsigned int>(network["shape"]); shape.shrink_to_fit();
             vector<double> bias = jcv::to_vector<double>(network["bias"]); bias.shrink_to_fit();
             vector<double> weights = jcv::to_vector<double>(network["weights"]); weights.shrink_to_fit();
-            Values values;
-            values.shape = shape;
-            values.bias = bias;
-            values.weights = weights;
+            Values values(shape, weights, bias);
             return values;
         }
 
@@ -259,10 +267,18 @@ namespace nn {
          * \brief Load `nn::Values` under an id.
          * `write_data()` must be called to update the file
         */
-        void load_values(Values n_, string id_) {
+        void load_values(Values& n_, string id_) {
             data[id_]["shape"] = jcv::from_vector<unsigned int>(n_.shape);
             data[id_]["bias"] = jcv::from_vector<double>(n_.bias);
             data[id_]["weights"] = jcv::from_vector<double>(n_.weights);
+        }
+        /**
+         * \brief Load `nn::Network` under an id.
+         * `write_data()` must be called to update the file
+        */
+        void load_values(Network& n_, string id_) {
+            auto v = n_.package_values();
+            load_values(v, id_);
         }
     };
     class Display {
@@ -283,7 +299,7 @@ namespace nn {
          * \brief Display `nn::Values` in a window
          * \param n_ Values to display
         */
-        void plot_network(Values n_) const {
+        void plot_network(Network& n_) const {
 
             sf::RenderWindow window(sf::VideoMode(_winx, _winy), _title, sf::Style::Close);
 
@@ -291,26 +307,30 @@ namespace nn {
             sf::Event event;
             window.clear(sf::Color::White);
 
-            unsigned int layer_count = n_.shape.size();
-            unsigned int node_r = 5;
+            auto shape = n_.shape();
+            auto weights = n_.weights();
+            auto bias = n_.bias();
+            unsigned int layer_count = shape.size();
+
+            const unsigned int node_r = 5;
 
             sf::CircleShape node(node_r);
             sf::VertexArray weight(sf::Lines, 2);
             unsigned int bias_index = 0;
             unsigned int weights_index = 0;
             for (unsigned int l = 0; l < layer_count; l++) {
-                unsigned int node_count = n_.shape[l];
+                unsigned int node_count = shape[l];
                 unsigned int nx = ((l + 1)*_winx) / (layer_count + 1);
                 for (unsigned int n = 0; n < node_count; n++) {
                     unsigned int ny = ((n + 1)*_winy) / (node_count + 1);
                     weight[0].position = sf::Vector2f(nx,ny);
                     if (l < (layer_count - 1)) {
                         unsigned int n2x = ((l + 2)*_winx) / (layer_count + 1);
-                        for (unsigned int w = 0; w < n_.shape[l+1]; w++) {
-                            unsigned int n2y = ((w + 1)*_winy) / (n_.shape[l+1] + 1);
+                        for (unsigned int w = 0; w < shape[l+1]; w++) {
+                            unsigned int n2y = ((w + 1)*_winy) / (shape[l+1] + 1);
                             weight[1].position = sf::Vector2f(n2x, n2y);
 
-                            double s = sig(n_.weights[weights_index]);
+                            double s = sig(weights[weights_index]);
                             weight[0].color = sf::Color(255*s, 255*-s, 255, 255);
                             weight[1].color = sf::Color(255*s, 255*-s, 255, 255);
 
@@ -319,7 +339,7 @@ namespace nn {
                             weights_index++;
                         }
                     }
-                    double s = sig(n_.bias[bias_index]);
+                    double s = sig(bias[bias_index]);
                     node.setFillColor(sf::Color(255, 255*s, 255*(1-s), 255));
                     node.setPosition(nx - node_r, ny - node_r);
                     window.draw(node);
